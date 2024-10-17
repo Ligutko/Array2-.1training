caa26b2d07e3aef082a65c309c917770eb17e77d
ligutko <desantvitaliy@gmail.com>
2024-10-17 12:39:55 +0300

    Add project files.

---
+++ Array2 .1training/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Введення розміру масиву
+        Console.Write("Введіть розмір масиву: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] array = new int[n];
+        Random random = new Random();
+
+        // Заповнюємо масив випадковими числами у діапазоні від -50 до 50
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(-50, 51);
+        }
+
+        // Виведення вихідного масиву
+        Console.WriteLine("Вихідний масив:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        // Підрахунок кількості від'ємних елементів
+        int negativeCount = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < 0)
+            {
+                negativeCount++;
+            }
+        }
+
+        // Виведення результату
+        Console.WriteLine($"Кількість від'ємних елементів у масиві: {negativeCount}");
+    }
+}
